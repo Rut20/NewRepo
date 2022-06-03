ef872226fbbc8e80c3bcb7de42381192a1fb232f
malkamatel <40566m@gmail.com>
2022-06-03 13:18:09 +0300

    Add Client project files.

---
+++ ApiCombination/Controllers/CombinationController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCombination.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CombinationController : ControllerBase
+    {
+
+
+        private readonly ILogger<CombinationController> _logger;
+
+        public CombinationController(ILogger<CombinationController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        [Route("GetStartApi/{num}")]
+        public int GetStartApi(int num)
+        {
+            try {
+            return BL.Bissnes.StartApi(num);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("error in CombinationController function GetStartApi" + ex);
+
+            }
+        }
+    
+
+        [HttpGet]
+        [Route("GetNextApi")]
+        public IEnumerable<int> GetNextApi()
+        {
+            try
+            {
+                return BL.Bissnes.NextApi();
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("error in CombinationController function GetNextApi" + ex);
+
+            }
+        }
+
+        [HttpGet]
+        [Route("GetAllApi/{pageNumber}/{pageSize}")]
+        public async Task<dynamic> GetAllApi(int pageNumber, int pageSize)
+        {
+            try
+            {
+                return BL.Bissnes.GetCombination(pageNumber, pageSize);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("error in CombinationController function GetAllApi" + ex);
+            }
+        }
+    }
+}
